0011f99c96dc1ba93e37ac750d50ef56147d113f
maraisaferreira <ferreira.maraisacristina@gmail.com>
2021-08-15 12:54:16 -0300

    class, getters, setters, validação do ip

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 from classes.calc_ipv4 import CalcIPV4
 
-calc_ipv4 = CalcIPV4(ip='192.168.0.1', prefixo=24)
+calc_ipv4 = CalcIPV4(ip='192.168.0.1', prefix=24)
--- classes/calc_ipv4.py
+++ classes/calc_ipv4.py
@@ -1,2 +1,49 @@
+import re
+
+
 class CalcIPV4:
-    pass
+
+    def __init__(self, ip, mask=None, prefix=None):
+        self.ip = ip
+        self.mask = mask
+        self.prefix = prefix
+
+    #getters
+    @property
+    def ip(self):
+        return self._ip
+
+    @property
+    def mask(self):
+        return self._mask
+
+    @property
+    def prefix(self):
+        return self._prefix
+
+    #setters
+    @ip.setter
+    def ip(self, value):
+        if not self._ip_validator(value):
+            raise ValueError('Ip Inválido.')
+
+        self._ip = value
+
+    @mask.setter
+    def mask(self, value):
+        self._mask = value
+
+    @prefix.setter
+    def prefix(self, value):
+        self._prefix = value
+
+    @staticmethod
+    def _ip_validator(ip):
+        regexp = re.compile(
+            r'^([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})$'
+        )
+
+        if regexp.search(ip):
+            return True
+
+

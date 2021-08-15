5b1c343b34e1e659889ad00cfa8d7e4bb1788644
maraisaferreira <ferreira.maraisacristina@gmail.com>
2021-08-15 16:54:16 -0300

    validação ip/mask/prefix - convet mask p/ binario

--- classes/calc_ipv4.py
+++ classes/calc_ipv4.py
@@ -25,16 +25,35 @@ class CalcIPV4:
     @ip.setter
     def ip(self, value):
         if not self._ip_validator(value):
-            raise ValueError('Ip Inválido.')
+            raise ValueError('IP Inválido.')
 
         self._ip = value
 
     @mask.setter
     def mask(self, value):
+
+        if not value:
+            return
+
+        if not self._ip_validator(value):
+            raise ValueError('Máscara Inválida.')
+
         self._mask = value
+        self._binary_mask = self.ip_to_binary(self.mask)
+        print(self._binary_mask)
 
     @prefix.setter
     def prefix(self, value):
+
+        if not value:
+            return
+
+        if not isinstance(value, int):
+            raise TypeError('O prefixo deve ser um número inteiro.')
+
+        if value > 32:
+            raise ValueError('O prefixo deve ter 32Bits.')
+
         self._prefix = value
 
     @staticmethod
@@ -46,4 +65,8 @@ class CalcIPV4:
         if regexp.search(ip):
             return True
 
-
+    @staticmethod
+    def ip_to_binary(ip):
+        groups = ip.split('.')
+        binary_groups = [bin(int(x))[2:].zfill(8) for x in groups]
+        return ''.join(binary_groups)

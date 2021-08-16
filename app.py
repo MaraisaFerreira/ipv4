c03553fbf319d23160ad9126b19a8fe3d6aee79c
maraisaferreira <ferreira.maraisacristina@gmail.com>
2021-08-16 12:01:12 -0300

    add final calcs - rede | broadcast | qtd ips

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from classes.calc_ipv4 import CalcIPV4
 
 calc_ipv4 = CalcIPV4(ip='192.168.0.1', mask='255.255.255.0')
+print(f'IP: {calc_ipv4.ip}')
+print(f'Máscara: {calc_ipv4.mask}')
+print(f'Prefixo: {calc_ipv4.prefix}')
+print(f'Rede: {calc_ipv4.net}')
+print(f'Broadcast: {calc_ipv4.broadcast}')
+print(f'Qtds IPs: {calc_ipv4.ips_amount}')
--- classes/calc_ipv4.py
+++ classes/calc_ipv4.py
@@ -8,7 +8,10 @@ class CalcIPV4:
         self.mask = mask
         self.prefix = prefix
 
-    #getters
+        self._set_broadcast()
+        self._set_net()
+
+    # getters
     @property
     def ip(self):
         return self._ip
@@ -21,17 +24,30 @@ class CalcIPV4:
     def prefix(self):
         return self._prefix
 
-    #setters
+    @property
+    def broadcast(self):
+        return self._broadcast
+
+    @property
+    def net(self):
+        return self._net
+
+    @property
+    def ips_amount(self):
+        return self._get_ips_amount()
+
+    # setters
+
     @ip.setter
     def ip(self, value):
         if not self._ip_validator(value):
             raise ValueError('IP Inválido.')
 
         self._ip = value
+        self._ip_binary = self._ip_to_binary(value)
 
     @mask.setter
     def mask(self, value):
-
         if not value:
             return
 
@@ -39,12 +55,13 @@ class CalcIPV4:
             raise ValueError('Máscara Inválida.')
 
         self._mask = value
-        self._binary_mask = self.ip_to_binary(self.mask)
-        print(self._binary_mask)
+        self._binary_mask = self._ip_to_binary(value)
+
+        if not hasattr(self, 'prefix'):
+            self.prefix = self._binary_mask.count('1')
 
     @prefix.setter
     def prefix(self, value):
-
         if not value:
             return
 
@@ -55,6 +72,10 @@ class CalcIPV4:
             raise ValueError('O prefixo deve ter 32Bits.')
 
         self._prefix = value
+        self._binary_mask = (value * '1').ljust(32, '0')
+
+        if not hasattr(self, 'mask'):
+            self.mask = self._binary_to_ip(self._binary_mask)
 
     @staticmethod
     def _ip_validator(ip):
@@ -66,7 +87,33 @@ class CalcIPV4:
             return True
 
     @staticmethod
-    def ip_to_binary(ip):
+    def _ip_to_binary(ip):
         groups = ip.split('.')
         binary_groups = [bin(int(x))[2:].zfill(8) for x in groups]
         return ''.join(binary_groups)
+
+    @staticmethod
+    def _binary_to_ip(binary):
+        n = 8
+
+        groups = [str(int(binary[i:i+n], 2)) for i in range(0, 32, n)]
+
+        return '.'.join(groups)
+
+    def _set_broadcast(self):
+        host_bits = 32 - self.prefix
+        self._broadcast_binary = self._ip_binary[:self.prefix] + (
+            host_bits * '1')
+
+        self._broadcast = self._binary_to_ip(self._broadcast_binary)
+        return self._broadcast
+
+    def _set_net(self):
+        host_bits = 32 - self.prefix
+        self._net_binary = self._ip_binary[:self.prefix] + (host_bits * '0')
+
+        self._net = self._binary_to_ip(self._net_binary)
+        return self._net
+
+    def _get_ips_amount(self):
+        return 2 ** (32 - self.prefix)
